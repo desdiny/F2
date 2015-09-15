f88b175b1fb17ce015acaa0b8d05a9319a95656d
Trolle <trolle.geuna@gmail.com>
2015-09-15 16:57:50 +0200

    start of datatype

--- F2.hs
+++ F2.hs
@@ -1,7 +1,15 @@
 
 
-data MolSeq = DNA String String | PROTEIN String String
+--data MolSeq = DNA String String | PROTEIN String String
+
+
+data MolSeq = MolSeq { sekvensnamn :: String, sekvens :: String, typ :: String}deriving(Show)
+
+data Typ = PROTEIN | DNA deriving(Show)
 
 
 
 
+String->String->Bool
+| if dna = MolSeq DNA a b
+| otherwise = MolSeq PROTEIN a b
