17c0307605784b923094ba7d8f0a717f29275075
oskarcasselryd <oskarcasselryd@gmail.com>
2015-09-15 16:31:35 +0200

    module F2 where

--- F2.hs
+++ F2.hs
@@ -1,3 +1,6 @@
+module F2 where
+
+<<<<<<< Updated upstream
 
 
 data MolSeq = DNA String String | PROTEIN String String
@@ -5,3 +8,9 @@ data MolSeq = DNA String String | PROTEIN String String
 
 
 
+=======
+module F2 where
+
+
+data MolSeq = DNA String String | PROTEIN String String
+>>>>>>> Stashed changes
