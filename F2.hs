2f962d0a3ef26cbb43c32b03ecdd26dc859f500a
oskarcasselryd <oskarcasselryd@gmail.com>
2015-09-15 17:48:40 +0200

    fixade seqLength

--- F2.hs
+++ F2.hs
@@ -10,12 +10,12 @@ data MolSeq = MolSeq { sekvensnamn :: String, sekvens :: String, typ :: Typ}deri
 
 dna =['A','C','G','T']
 
-string2seq:: String->String->MolSeq
+string2seq :: String->String->MolSeq
 string2seq namn sekvens
   | checkDNA sekvens = MolSeq namn sekvens DNA
   | otherwise = MolSeq namn sekvens PROTEIN
 
-checkDNA:: String -> Bool
+checkDNA :: String -> Bool
 checkDNA [] = True
 checkDNA(h:t)
   | elem h dna = checkDNA(t)
@@ -30,5 +30,10 @@ seqName m = sekvensnamn m
 seqSequence :: MolSeq -> String
 seqSequence m = sekvens m
 
-seqLength :: MolSeq -> Typ
-seqLength m = typ m
+seqLength :: MolSeq -> Int
+seqLength m = length (sekvens m)
+
+
+
+
+--seqDistance :: MolSeq -> MolSeq -> Double
